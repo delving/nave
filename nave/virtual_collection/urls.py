d39c809e44666899f150506f5209a7a13db86e72
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2016-04-27 12:46:04 +0200

    Fix for routing issues with search page in the virtual collections

--- nave/virtual_collection/urls.py
+++ nave/virtual_collection/urls.py
@@ -5,10 +5,11 @@ from . import views
 
 urlpatterns = patterns(
     '',
-    url(r'^vc/(?P<slug>(.*?))/$', views.VirtualCollectionDetailView.as_view(),
-        name="virtual_collection_detail"),
-    url(r'^vc/(?P<slug>(.*?))/search/$', views.VirtualCollectionSearchView.as_view(),
+    url(r'^vc/(?P<slug>([^/]*?))/search/$', views.VirtualCollectionSearchView.as_view(),
         name="virtual_collection_search"),
+    url(r'^vc/(?P<slug>([^/]*?))/$', views.VirtualCollectionDetailView.as_view(),
+        name="virtual_collection_detail"),
+
     # url for api
     # url for oai-pmh
 )
