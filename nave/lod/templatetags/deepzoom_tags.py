e4cc7b26d9bd4d87da3276ec978d77089ff08b84
Zemyatin <eric@delving.eu>
2016-06-10 14:05:37 +0200

    make viewer_id configurable

--- nave/lod/templatetags/deepzoom_tags.py
+++ nave/lod/templatetags/deepzoom_tags.py
@@ -6,7 +6,8 @@ register = template.Library()
 def deepzoom_js(
         deepzoom_count=0,
         deepzoom_urls=None,
-        show_navigator=False):
+        show_navigator=False,
+        viewer_id="zoom_viewer"):
     """
     get zoom url count and values
     :return: count as integer and urls as array
@@ -16,20 +17,17 @@ def deepzoom_js(
         'deepzoom_count': deepzoom_count,
         'deepzoom_urls': deepzoom_urls,
         'show_navigator': show_navigator,
+        'viewer_id': viewer_id,
     }
 
 @register.inclusion_tag('rdf/tags/_deepzoom_viewer.html')
 def deepzoom_viewer(
-        deepzoom_count=0,
-        deepzoom_urls=None,
-        show_navigator=False):
+        viewer_id="zoom_viewer"
+    ):
     """
     get zoom url count and values
     :return: count as integer and urls as array
     """
-
     return {
-        'deepzoom_count': deepzoom_count,
-        'deepzoom_urls': deepzoom_urls,
-        'show_navigator': show_navigator,
+        'viewer_id': viewer_id,
     }
