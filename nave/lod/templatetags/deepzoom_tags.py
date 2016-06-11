02a92d9c569fb1f0205582ab02103f3b87539d22
Zemyatin <eric@delving.eu>
2016-06-11 13:54:39 +0200

    place bootstrap responsive divs outside of deepzoom tag template and stick with the bare minimum. Make tags more configurable.

--- nave/lod/templatetags/deepzoom_tags.py
+++ nave/lod/templatetags/deepzoom_tags.py
@@ -7,6 +7,8 @@ def deepzoom_js(
         deepzoom_count=0,
         deepzoom_urls=None,
         show_navigator=False,
+        navigator_position="TOP_LEFT",
+        toolbar_id="zoom_navigation",
         viewer_id="zoom_viewer"):
     """
     get zoom url count and values
@@ -17,17 +19,21 @@ def deepzoom_js(
         'deepzoom_count': deepzoom_count,
         'deepzoom_urls': deepzoom_urls,
         'show_navigator': show_navigator,
+        'navigator_position': navigator_position,
+        'toolbar_id': toolbar_id,
         'viewer_id': viewer_id,
     }
 
+
 @register.inclusion_tag('rdf/tags/_deepzoom_viewer.html')
 def deepzoom_viewer(
-        viewer_id="zoom_viewer"
-    ):
+        viewer_id="zoom_viewer",
+        viewer_class="embed-responsive-item"):
     """
     get zoom url count and values
     :return: count as integer and urls as array
     """
     return {
         'viewer_id': viewer_id,
+        'viewer_class': viewer_class,
     }
