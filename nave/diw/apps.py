2d9f11816c4f27bc53089c960a2a2637944dc0b9
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2016-09-16 12:07:00 +0200

    Updated the DiW app to include the nave namespace in imports

---
+++ nave/diw/apps.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*- 
+"""
+This module configures the Human readable name for this module.
+
+It will also be used in the admin section.
+"""
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class DiwConfig(AppConfig):
+    name = 'nave.diw'
+    verbose_name = _("Delving Instant Website module")
