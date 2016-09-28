7c1539d5cf04f72fad40c09f835806c709945136
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2016-09-28 19:56:22 +0200

    Added nave namespace

--- nave/__init__.py
+++ nave/__init__.py
@@ -15,6 +15,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 # Also add information on how to contact you by electronic and paper mail.
 
-from common import version
+from nave.common import version
 
 __version__ = version.__version__
