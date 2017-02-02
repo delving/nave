bdc50dc721baa018ce9e8bbfb10e75574ef21651
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2017-02-02 11:59:11 +0100

    Update test for nave.search queryparser.

--- nave/search/tests/test_queryparser.py
+++ nave/search/tests/test_queryparser.py
@@ -1,8 +1,5 @@
-# -*- coding: utf-8 -*- 
-"""This module does
-
-
-"""
+# -*- coding: utf-8 -*-
+""" Test Nave QueryParser."""
 from functools import partial
 
 from django.test import TestCase
@@ -10,28 +7,33 @@ from django.test import TestCase
 from search.search import NaveESQuery
 
 
-class TestQueryParser(TestCase):
+def test_normal_query_string():
+    response = NaveESQuery._is_fielded_query("A query")
+    assert response is not None
+    assert not response
+    assert not NaveESQuery._is_fielded_query("one and two")
 
-    def test_normal_query_string(self):
-        response = NaveESQuery._is_fielded_query("A query")
-        assert response is not None
-        assert not response
-        assert not NaveESQuery._is_fielded_query("one and two")
 
-    def test_fielded_query(self):
-        response = NaveESQuery._is_fielded_query("dc_subject.value:bloemen")
-        assert response is not None
-        assert response
-        assert NaveESQuery._is_fielded_query("_id:123")
-        assert NaveESQuery._is_fielded_query("one AND two")
-        assert NaveESQuery._is_fielded_query("rdf.object.value:bloemen")
+def test_fielded_query():
+    response = NaveESQuery._is_fielded_query("dc_subject.value:bloemen")
+    assert response is not None
+    assert response
+    assert NaveESQuery._is_fielded_query("_id:123")
+    assert NaveESQuery._is_fielded_query("one AND two")
+    assert NaveESQuery._is_fielded_query("rdf.object.value:bloemen")
 
-    def test_query_cleanup(self):
-        assert NaveESQuery._created_fielded_query(query_string="dc_subject_facet:bloem") == "dc_subject.raw:bloem"
-        assert NaveESQuery._created_fielded_query(query_string="dc_subject_string:bloem") == "dc_subject.raw:bloem"
-        assert NaveESQuery._created_fielded_query(query_string="dc_subject_text:bloem") == "dc_subject.value:bloem"
-        assert NaveESQuery._created_fielded_query(query_string="dc_subject.value:bloem") == "dc_subject.value:bloem"
 
-    def test_multi_string_query_cleanup(self):
-        query = partial(NaveESQuery._created_fielded_query)
-        assert query(query_string="dc_subject_facet:bloem AND dc_title_text:roos") == "dc_subject.raw:bloem AND dc_title.value:roos"
+def test_query_cleanup():
+    query = NaveESQuery()
+    assert query._created_fielded_query(query_string="dc_subject_facet:bloem") == "dc_subject.raw:bloem"
+    assert query._created_fielded_query(query_string="dc_subject_string:bloem") == "dc_subject.raw:bloem"
+    assert query._created_fielded_query(query_string="dc_subject_text:bloem") == "dc_subject.value:bloem"
+    assert query._created_fielded_query(query_string="dc_subject.value:bloem") == "dc_subject.value:bloem"
+
+
+def test_multi_string_query_cleanup():
+    query = NaveESQuery()
+    res = query._created_fielded_query(
+        query_string="dc_subject_facet:bloem AND dc_title_text:roos"
+    )
+    res == "dc_subject.raw:bloem AND dc_title.value:roos"
