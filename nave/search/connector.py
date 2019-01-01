f2af17d4f06aa70343003f9093aaeb43982a4b59
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2019-01-01 18:44:04 +0100

    Use default connection settings for ElasticSearch connection pool.

--- nave/search/connector.py
+++ nave/search/connector.py
@@ -62,11 +62,11 @@ es_client = None
 try:
     connections.create_connection(
         hosts=ES_URLS,
-        sniff_on_start=False,
-        sniff_on_connection_fail=False,
-        sniffer_timeout=60,
-        maxsize=10,  # default value 10
-        retry_on_timeout=True,
+        # sniff_on_start=False,
+        # sniff_on_connection_fail=False,
+        # sniffer_timeout=60,
+        # maxsize=10,  # default value 10
+        # retry_on_timeout=True,
         timeout=ES_TIMEOUT,
     )
 except (ConnectionError, TransportError) as ce:
@@ -79,11 +79,11 @@ except (ConnectionError, TransportError) as ce:
 def get_es_client():
     return Elasticsearch(
         hosts=ES_URLS,
-        sniff_on_start=False,
-        sniff_on_connection_fail=False,
-        sniffer_timeout=60,
-        maxsize=25,  # default value 10
-        retry_on_timeout=True,
+        # sniff_on_start=False,
+        # sniff_on_connection_fail=False,
+        # sniffer_timeout=60,
+        # maxsize=25,  # default value 10
+        # retry_on_timeout=True,
         timeout=ES_TIMEOUT,
     )
 
